50d681a7635b5f5bcc83009963a7874efe972f8b
HRK-KY <humansequencer0@gmail.com>
2020-09-12 12:39:02 +0900

    implement user registration function with wizard style

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :postnumber, :prefecture, :city, :house_number, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+
   root to: 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :addresses , only: [:new, :create ]
+  # resources :addresses , only: [:new, :create ]
 
 end
