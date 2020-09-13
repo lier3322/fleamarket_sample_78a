17398142ac38cc68176cc709dd48d4449d331b99
HRK-KY <humansequencer0@gmail.com>
2020-09-13 13:53:13 +0900

    set validations and show flash message when error occured

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  has_one :address
+  validates :nickname, :birthday, presence: true
+  validates :first_name, :last_name, presence: true,
+    format: {
+      with: /\A[ぁ-んァ-ン一-龥]/,
+      message: "は全角（ひらがな、カタカナ、漢字）のみで入力して下さい"
+    }
+  validates :first_name_kana, :last_name_kana, presence: true,
+    format: {
+      with: /\A[ァ-ヶー－]+\z/,
+      message: "は全角カタカナのみで入力して下さい"
+    }
+
+    has_one :address
 end
