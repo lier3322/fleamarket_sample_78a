d24aaa56b4cd26db3c401e408e08df04d751277b
lier3322 <baoguix@gmail.com>
2020-09-29 16:42:37 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :mypages, only: [:show, :index]
   resources :creditcards, only: [:new, :create, :edit, :update]
-end
-  # resources :users, only: [:index]
+
 
   # resources :products, except: :show  
   # resources :addresses , only: [:new, :create ]
